1b88d194fb49ca64054fa7782d1b52080fd05170
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-02-21 14:45:20 -0500

    Update app.js
    update pugObj to renderPug

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-	response.render('index', {title: pug.pugObj.title});
+	response.render('index', {title: pug.renderPug.title});
 })
 
 app.listen('3000', () => {
