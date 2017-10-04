9e63acc5b2631bc884dd138793e7e56a8749bd17
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-10-03 22:00:40 -0400

    updated directory structure for create-react-app

--- local-weather/src/App.js
+++ local-weather/src/App.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react'
-import './App.css'
-import WeatherDetails from './WeatherDetails'
+import React, { Component } from 'react';
+import './App.css';
+import WeatherDetails from './WeatherDetails';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+      tempFarenheit: 0,
+    };
+  }
+  componentDidMount() {
+    axios.get('http://ipinfo.io').then(response => {
+      const latitude = Math.ceil(response.data.loc.slice(0, 6));
+      const longitude = Math.ceil(response.data.loc.slice(8, 15));
+      const baseUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=b8d68c8a65b14ad16c7c153dca2c7882&units=imperial`;
+      axios.get(baseUrl).then(response => {
+        console.log(response.data);
+        this.setState({
+          cityName: response.data.name,
+          tempFarenheit: response.data.main.temp,
+        });
+      });
+    });
+  }
+
   render() {
-    return <WeatherDetails />
+    return <WeatherDetails name={this.state.cityName} farenheit={this.state.tempFarenheit} />;
   }
 }
 
-export default App
+export default App;
--- local-weather/src/WeatherDetails.js
+++ local-weather/src/WeatherDetails.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-class WeatherDetails extends Component {
-  render() {
-    return (
-      <div className="col-md-6 col-md-offset-3">
-        <div className="main-output text-center well well-lg">
-          <h1 className="page-header text-center"> </h1>
-          <p id="weather-description" />
-          <i className="icons" />
-          <p id="weather-icon" />
-          <p id="city-name" />
-          <p id="city-weather" />
-          <p id="weather-sub-info" className="text-center" />
-          <button id="fahr" className="btn btn-default">
-            Farenheit
-          </button>
-          <button id="cels" className="btn btn-default" />
-          <button id="advSearch" className="btn btn-default">
-            Search by zip
-          </button>
-        </div>
+const WeatherDetails = props => {
+  return (
+    <div className="col-md-6 col-md-offset-3">
+      <div className="main-output text-center well well-lg">
+        <h1 className="page-header text-center">{props.name}</h1>
+        <p id="weather-description" />
+        <i className="icons" />
+        <p id="weather-icon" />
+        <p id="city-name" />
+        <p id="city-weather">{props.farenheit} degrees farenheit</p>
+        <p id="weather-sub-info" className="text-center" />
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default WeatherDetails
+export default WeatherDetails;
