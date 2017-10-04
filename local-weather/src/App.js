7208740d5483da2e6d9a2f9e6a5900fa2c259c44
Rakeem Thomas <thomas.rakeem.29@gmail.com>
2017-10-03 22:58:23 -0400

    completed info for WeatherDetails component

--- local-weather/src/App.js
+++ local-weather/src/App.js
@@ -1,34 +1,50 @@
-import React, { Component } from 'react';
-import './App.css';
-import WeatherDetails from './WeatherDetails';
-import axios from 'axios';
+import React, { Component } from 'react'
+import './App.css'
+import WeatherDetails from './WeatherDetails'
+import axios from 'axios'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       cityName: '',
+      region: '',
       tempFarenheit: 0,
-    };
+      minTemp: 0,
+      maxTemp: 0,
+    }
   }
   componentDidMount() {
     axios.get('http://ipinfo.io').then(response => {
-      const latitude = Math.ceil(response.data.loc.slice(0, 6));
-      const longitude = Math.ceil(response.data.loc.slice(8, 15));
-      const baseUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=b8d68c8a65b14ad16c7c153dca2c7882&units=imperial`;
+      this.setState({
+        cityName: response.data.city,
+        region: response.data.region,
+      })
+      const latitude = Math.ceil(response.data.loc.slice(0, 6))
+      const longitude = Math.ceil(response.data.loc.slice(8, 15))
+      const baseUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=b8d68c8a65b14ad16c7c153dca2c7882&units=imperial`
       axios.get(baseUrl).then(response => {
-        console.log(response.data);
+        console.log(response.data)
         this.setState({
-          cityName: response.data.name,
           tempFarenheit: response.data.main.temp,
-        });
-      });
-    });
+          minTemp: response.data.main.temp_min,
+          maxTemp: response.data.main.temp_max,
+        })
+      })
+    })
   }
 
   render() {
-    return <WeatherDetails name={this.state.cityName} farenheit={this.state.tempFarenheit} />;
+    return (
+      <WeatherDetails
+        name={this.state.cityName}
+        farenheit={this.state.tempFarenheit}
+        minTemp={this.state.minTemp}
+        maxTemp={this.state.maxTemp}
+        region={this.state.region}
+      />
+    )
   }
 }
 
-export default App;
+export default App
--- local-weather/src/WeatherDetails.js
+++ local-weather/src/WeatherDetails.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React from 'react'
 
 const WeatherDetails = props => {
   return (
     <div className="col-md-6 col-md-offset-3">
       <div className="main-output text-center well well-lg">
-        <h1 className="page-header text-center">{props.name}</h1>
+        <h1 className="page-header text-center">
+          {props.name}, {props.region}
+        </h1>
         <p id="weather-description" />
         <i className="icons" />
         <p id="weather-icon" />
         <p id="city-name" />
-        <p id="city-weather">{props.farenheit} degrees farenheit</p>
+        <p id="city-weather">Current Temperature: {props.farenheit} degrees farenheit</p>
+        <p id="city-weather">Min: {props.minTemp} degrees farenheit</p>
+        <p id="city-weather">Max: {props.maxTemp} degrees farenheit</p>
         <p id="weather-sub-info" className="text-center" />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default WeatherDetails;
+export default WeatherDetails
